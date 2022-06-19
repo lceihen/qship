1786237f0367a05032a5c4ae738cfacffb86c819
lceihen <2750418124@qq.com>
2022-06-20 01:29:14 +0800

    css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import './App.scss'
+import Test from './views/test/index.tsx'
 const App = () => {
-  return <div>App</div>
+  return <div>12</div>
 }
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+  //@ts-ignore
   <React.StrictMode>
     <App />
   </React.StrictMode>

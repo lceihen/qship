68b7b0d9bdd512cf5aa24226b6941c6325200f79
lceihen <2750418124@qq.com>
2022-06-20 14:38:31 +0800

    postcss autoprefixer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.scss'
+import './App1.css'
 import Test from './views/test/index.tsx'
 const App = () => {
   return <div>12</div>
--- client/webpack/webpack.config.js
+++ client/webpack/webpack.config.js
@@ -1,12 +1,46 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const getStyleLoaders = (preProcessor) => {
+  return [
+    'style-loader',
+    'css-loader',
+    {
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: {
+          plugins: [
+            [
+              'postcss-preset-env',
+              {
+                autoprefixer: {
+                  flexbox: 'no-2009',
+                },
+                stage: 3,
+              },
+            ],
+          ],
+        },
+      },
+    },
+    ,
+    preProcessor,
+  ].filter(Boolean)
+}
 module.exports = {
   entry: path.resolve(__dirname, '../src/main.tsx'),
   module: {
     rules: [
       {
-        test: /.(ts|js|tsx|ts)$/,
+        test: /\.(ts|js|tsx|ts)$/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: getStyleLoaders(''),
+      },
+      {
+        test: /\.(scss|sass)$/i,
+        use: getStyleLoaders('sass-loader'),
       },
     ],
   },
@@ -22,5 +56,8 @@ module.exports = {
     filename: 'bundle.[chunkhash].js',
     path: path.resolve(__dirname, '../dist'),
     clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 }
